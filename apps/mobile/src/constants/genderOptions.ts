35888367ccf49f046af2316ffffadb36712e0a65
rodrigololr <rodrigotenorio2005@gmail.com>
2026-05-18 20:32:48 -0300

    feat(onboarding): add types and constants for mobile app

---
+++ apps/mobile/src/constants/genderOptions.ts
@@ -0,0 +1,8 @@
+import { SelectOption } from "@/types";
+
+export const genderOptions: SelectOption[] = [
+  { label: "Feminino", value: "Feminino" },
+  { label: "Masculino", value: "Masculino" },
+  { label: "Não-binário", value: "Não-binário" },
+  { label: "Prefiro não informar", value: "Prefiro não informar" },
+];
