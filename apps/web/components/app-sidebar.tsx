32222e68e0e8056628fdbcfeddb8e6497353ae7c
Eduardo Maciel <eduardomacielbr@gmail.com>
2026-05-21 02:45:37 -0300

    style: improve collapse animation smoothness further more

--- apps/web/components/app-sidebar.tsx
+++ apps/web/components/app-sidebar.tsx
@@ -69,14 +69,14 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 		<Sidebar collapsible="icon" {...props}>
 			<SidebarHeader>
 				<SidebarMenu>
-					<SidebarMenuItem className="flex flex-col items-start gap-2 group-data-[collapsible=icon]:gap-0 pt-2">
-						<div className="flex w-full flex-row items-center justify-between gap-2 group-data-[collapsible=icon]:w-fit group-data-[collapsible=icon]:justify-start group-data-[collapsible=icon]:gap-0">
-							<div className="w-31.5 shrink-0 overflow-hidden transition-[width,opacity] duration-200 group-data-[collapsible=icon]:w-0 group-data-[collapsible=icon]:opacity-0">
-								<Logo className="h-5 w-31.5 max-w-none pl-4" />
+					<SidebarMenuItem className="flex flex-col items-start group-data-[collapsible=icon]:gap-0 pt-2">
+						<div className="flex w-full flex-row items-center justify-between transition-all duration-300 ease-in-out">
+							<div className="w-36 shrink-0 overflow-hidden pl-4 transition-[width,opacity,padding] duration-300 ease-in-out group-data-[collapsible=icon]:w-0 group-data-[collapsible=icon]:pl-0 group-data-[collapsible=icon]:opacity-0">
+								<Logo className="h-5 w-31.5 max-w-none" />
 							</div>
 							<SidebarTrigger />
 						</div>
-						<span className="max-h-5 overflow-hidden text-sm font-normal text-muted-foreground transition-[max-height,opacity,transform,margin-top] duration-200 group-data-[collapsible=icon]:pointer-events-none group-data-[collapsible=icon]:-mt-2 group-data-[collapsible=icon]:max-h-0 group-data-[collapsible=icon]:opacity-0 pl-4">
+						<span className="max-h-5 overflow-hidden text-sm font-normal text-muted-foreground transition-[max-height,opacity,transform,margin-top] duration-300 ease-in-out group-data-[collapsible=icon]:pointer-events-none group-data-[collapsible=icon]:-mt-2 group-data-[collapsible=icon]:max-h-0 group-data-[collapsible=icon]:opacity-0 pl-4 mt-2">
 							Painel NAC
 						</span>
 					</SidebarMenuItem>
