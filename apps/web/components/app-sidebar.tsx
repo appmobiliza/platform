db99e71bf999186ba4cc177a1446bfc18bb8ae66
Eduardo Maciel <eduardomacielbr@gmail.com>
2026-05-21 02:24:29 -0300

    style: improve collapse animation even more

--- apps/web/components/app-sidebar.tsx
+++ apps/web/components/app-sidebar.tsx
@@ -69,21 +69,21 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 		<Sidebar collapsible="icon" {...props}>
 			<SidebarHeader>
 				<SidebarMenu>
-					<SidebarMenuItem className="flex flex-col items-start gap-2 group-data-[collapsible=icon]:gap-0">
+					<SidebarMenuItem className="flex flex-col items-start gap-2 group-data-[collapsible=icon]:gap-0 pt-2">
 						<div className="flex w-full flex-row items-center justify-between gap-2 group-data-[collapsible=icon]:w-fit group-data-[collapsible=icon]:justify-start group-data-[collapsible=icon]:gap-0">
 							<div className="w-31.5 shrink-0 overflow-hidden transition-[width,opacity] duration-200 group-data-[collapsible=icon]:w-0 group-data-[collapsible=icon]:opacity-0">
-								<Logo className="h-5 w-31.5 max-w-none" />
+								<Logo className="h-5 w-31.5 max-w-none pl-4" />
 							</div>
 							<SidebarTrigger />
 						</div>
-						<span className="max-h-5 overflow-hidden text-sm font-normal text-muted-foreground transition-[max-height,opacity,transform,margin-top] duration-200 group-data-[collapsible=icon]:pointer-events-none group-data-[collapsible=icon]:-mt-2 group-data-[collapsible=icon]:max-h-0 group-data-[collapsible=icon]:opacity-0">
+						<span className="max-h-5 overflow-hidden text-sm font-normal text-muted-foreground transition-[max-height,opacity,transform,margin-top] duration-200 group-data-[collapsible=icon]:pointer-events-none group-data-[collapsible=icon]:-mt-2 group-data-[collapsible=icon]:max-h-0 group-data-[collapsible=icon]:opacity-0 pl-4">
 							Painel NAC
 						</span>
 					</SidebarMenuItem>
 				</SidebarMenu>
 			</SidebarHeader>
 			<SidebarContent>
-				<div className="border-b border-border pt-4 text-sm font-medium" />
+				<div className="border-b border-border pt-3 text-sm font-medium" />
 				<NavMain items={data.navMain} />
 			</SidebarContent>
 			<SidebarFooter>
