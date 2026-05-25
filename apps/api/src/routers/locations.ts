a929060810f8921170e6a0592140928f8339a08b
felipdsa21 <93343201+felipdsa21@users.noreply.github.com>
2026-05-25 16:43:39 -0300

    feat(db): add coordinates to location

--- apps/api/src/routers/locations.ts
+++ apps/api/src/routers/locations.ts
@@ -43,6 +43,8 @@ export const locationsRouter = router({
         name: z.string().min(2).max(100),
         abbreviation: z.string().min(1).max(20),
         description: z.string().max(500).optional(),
+        latitude: z.number(),
+        longitude: z.number(),
       }),
     )
     .mutation(async ({ input }) => {
