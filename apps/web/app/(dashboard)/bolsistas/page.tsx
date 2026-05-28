137fc1b045b171be305a7c3363821f6141aea94a
Eduardo Maciel <eduardomacielbr@gmail.com>
2026-05-28 19:04:42 -0300

    feat: add scholar dialog

--- apps/web/app/(dashboard)/bolsistas/page.tsx
+++ apps/web/app/(dashboard)/bolsistas/page.tsx
@@ -16,6 +16,8 @@ import { cn } from "@/lib/utils";
 
 import { dashboardCards, scholarsData } from "@/data/scholars-data";
 
+import { AddScholarDialog } from "./dialog/add-scholar";
+
 export const metadata: Metadata = {
 	title: "Bolsistas",
 };
@@ -37,10 +39,12 @@ export default function ScholarsPage() {
 						</h2>
 					</div>
 					<div className="flex flex-wrap items-center gap-4">
-						<Button size={"lg"} className="gap-2 px-3">
-							<Plus className="size-4" />
-							Adicionar bolsista
-						</Button>
+						<AddScholarDialog>
+							<Button size={"lg"} className="gap-2 px-3">
+								<Plus className="size-4" />
+								Adicionar bolsista
+							</Button>
+						</AddScholarDialog>
 					</div>
 				</header>
 
