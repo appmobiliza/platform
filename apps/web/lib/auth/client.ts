44ae555ff974a06188b234737bb8542a0cf18b76
Eduardo Maciel <eduardomacielbr@gmail.com>
2026-06-12 17:30:41 -0300

    test: try to fix API auth redirect

--- apps/web/lib/auth/client.ts
+++ apps/web/lib/auth/client.ts
@@ -3,6 +3,8 @@
 import { createAuthClient } from "@mobiliza/auth/client";
 import { apiBaseUrl } from "@mobiliza/env/base-url";
 
+console.log("apiBaseUrl", apiBaseUrl);
+
 export const authClient = createAuthClient({
 	baseURL: `${apiBaseUrl}/api/auth`,
 });
